ec916993e6810f686c57d6f62e17288299166a09
Isabel Ramos <iramos1226@gmail.com>
2017-05-02 19:25:13 -0500

    added email/password/username stuff

---
+++ javascripts/auth.js
@@ -0,0 +1,15 @@
+var firebaseApi = ((cats) => {
+
+	cats.registerUser = (credentials) => {
+		return new Promise((resolve, reject) => {
+			firebase.auth().createUserWithEmailAndPassword(credentials.email, credentials.password)
+			.then((authData) => {
+				resolve(authData);
+			}).catch((error) => {
+				reject(error);
+			});
+		});
+	};
+
+	return cats;
+})(firebaseApi || {});
---
+++ javascripts/userCrud.js
@@ -0,0 +1,6 @@
+var firebaseApi = ((cats) => {
+
+
+
+	return cats;
+})(firebaseApi || {});
